8dbf15b9cefab618c805326451d28f7880f46e6e
Bryan Laryea <bryan.laryea@telecel.com.gh>
2025-02-18 12:31:27 +0000

    adfas

--- src/main/java/com/serenity/integration/repository/InvoiceRepository.java
+++ src/main/java/com/serenity/integration/repository/InvoiceRepository.java
@@ -1,11 +1,16 @@
 package com.serenity.integration.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.serenity.integration.models.PatientInvoice;
 
 @Repository
 public interface InvoiceRepository extends JpaRepository<PatientInvoice,Long>{
+@Query(value="SELECT * FROM patient_invoice offset ?1 limit ?2")
+    List<PatientInvoice> findBatch(int startIndex, int batchSize);
 
 }
