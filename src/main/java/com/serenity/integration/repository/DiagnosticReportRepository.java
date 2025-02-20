3de164cb7f91a7b1eee9e3539ccb4fa9146efe54
Bryan Laryea <bryan.laryea@telecel.com.gh>
2025-02-20 10:11:16 +0000

    adfas

--- src/main/java/com/serenity/integration/repository/DiagnosticReportRepository.java
+++ src/main/java/com/serenity/integration/repository/DiagnosticReportRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import com.serenity.integration.models.DiagnosticReport;
 @Repository
 public interface DiagnosticReportRepository extends JpaRepository<DiagnosticReport,Long>{
-    @Query(value="SELECT * FROM diagnostic_report where encounterid is not null order By id OFFSET ?1 LIMIT 2",nativeQuery=true)
+    @Query(value="SELECT * FROM diagnostic_report where encounterid is not null order By id OFFSET ?1 LIMIT ?2",nativeQuery=true)
 List<DiagnosticReport> findBhy(int offset,int limit);
 @Query(value="SELECT count(*) FROM diagnostic_report where encounterid is not null",nativeQuery=true)
 long findCleanCount();
