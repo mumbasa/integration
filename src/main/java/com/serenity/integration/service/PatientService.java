d8a842284ddf4689510e92af0515d7ad53f4d1e1
Bryan Laryea <bryan.laryea@telecel.com.gh>
2025-03-10 17:08:14 +0000

    edited message

--- src/main/java/com/serenity/integration/service/PatientService.java
+++ src/main/java/com/serenity/integration/service/PatientService.java
@@ -592,7 +592,7 @@ serenityJdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
             int startIndex = i * batchSize;
             String sqlQuery = """
           SELECT p.id, p."uuid", p.created_at, p.modified_at, txid, ts, resource_type, p.status, resource, mr_number, birth_date, birth_time, blood_type, deceased_date_time, employer, first_name, gender, is_active, is_deceased,p.is_deleted, last_name, marital_status, meta, multiple_birth_boolean, multiple_birth_integer, name_prefix, occupation, other_names, religious_affiliation::text as religious_affiliation, photo, passport_number, general_practitioner_id, p.managing_organization_id, user_id, email, mobile, national_mobile_number, pa.uuid as previous_patient_account_uuid, previous_payment_method, is_hospitalized, admission_id,currency, current_visit_uuid
-FROM patient p join patient_account pa on pa."uuid" = uuid(p.previous_patient_account_uuid)
+FROM patient p left join patient_account pa on pa."uuid" = uuid(p.previous_patient_account_uuid)
 
             order by  p.id asc offset ? LIMIT ?
                      """;
@@ -671,15 +671,15 @@ FROM patient p join patient_account pa on pa."uuid" = uuid(p.previous_patient_ac
         
       
         
-     //  addressRepo.saveAll(address.values().stream().toList());
+      addressRepo.saveAll(address.values().stream().toList());
        //saveLegacyAddressInSerenity(address.values().stream().toList());
 
-     /*   persons.values().stream().forEach(e -> 
+       persons.values().stream().forEach(e -> 
        {
-        saveRelatedPersion(e);
+       /// saveRelatedPersion(e);
     relatedRepo.saveAll(e);
     }
-       ); */
+       );
 
     }
 
