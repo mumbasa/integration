b0027d89c9009b7401cdba87777b0f1071073f98
Bryan Laryea <bryan.laryea@telecel.com.gh>
2025-03-25 12:54:21 +0000

    added

--- src/main/java/com/serenity/integration/IntegrationApplication.java
+++ src/main/java/com/serenity/integration/IntegrationApplication.java
@@ -101,7 +101,7 @@ InvoiceService invoiceService;
 		}
 		long start = System.currentTimeMillis();
 		//patientService.getLegacyAllPatients(1000,1000);
-		//patientService.getLegacyAllPatients2(1000,1000);
+		patientService.getLegacyAllPatients2(1000,1000);
 		patientMigrationService.getPatientsThreads();
 		//encounterService.getLegacyEncounters(1000);
 		//observationService.getLegacyObservations(1000);
