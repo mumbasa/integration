574fb25685c093b285d1ef3e1dea0b2d712a4543
Bryan Laryea <bry_lar@yahoo.com>
2025-04-17 17:59:59 +0000

    added charge

--- src/main/java/com/serenity/integration/repository/ObservationRepository.java
+++ src/main/java/com/serenity/integration/repository/ObservationRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import com.serenity.integration.models.Observation;
 @Repository
 public interface ObservationRepository extends JpaRepository<Observation,Long>{
-    @Query(value = "SELECT * from observations o  where patientid is null order by id OFFSET ?1 LIMIT ?2",nativeQuery = true)
+    @Query(value = "SELECT * from observations o  where patientid is not null order by id OFFSET ?1 LIMIT ?2",nativeQuery = true)
 List<Observation> findByPractitionerIdNotNull(int offset,int limit);
 
 @Query(value = "SELECT count(*) from observations o",nativeQuery = true)
