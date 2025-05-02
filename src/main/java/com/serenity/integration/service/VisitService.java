67b20b1be2fb92857038454dad8da33f6e8e1d10
Bryan Laryea <bry_lar@yahoo.com>
2025-05-02 09:22:09 +0000

    aadeed adata

--- src/main/java/com/serenity/integration/service/VisitService.java
+++ src/main/java/com/serenity/integration/service/VisitService.java
@@ -374,7 +374,7 @@ return visits.size();
         List<Visits> visits = new ArrayList<>();
         String sql = """
                 SELECT  v.created_at, v.is_deleted, v.modified_at, v.id as uuid, v.status, visit_class, priority, arrived_at, ended_at, appointment_id, assigned_to_id, p.uuid as patient_id, service_provider_id, primary_location_id, next_encounter_due,  p.birth_date, p.email, p.first_name, p.gender, p.last_name, p.mobile,  p.other_names,  encounter_history, p.name_prefix as title,c.user_friendly_id as user_friendly_id
-FROM visit  v join patient p  on p.id = v.patient_id  left join  ChargeItem c on visit_id=uuid
+FROM visit  v join patient p  on p.id = v.patient_id  left join  "ChargeItem" c on visit_id=uuid
                 order by v.created_at offset ? limit ?
                 """;;
         SqlRowSet set = legJdbcTemplate.queryForRowSet(sql,startIndex,batchSize);
