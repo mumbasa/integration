fb1c82effa11494037946ed0609c17693228f2f8
Bryan Laryea <bry_lar@yahoo.com>
2025-05-23 00:47:22 +0000

    added cvariables to the service

--- src/main/java/com/serenity/integration/IntegrationApplication.java
+++ src/main/java/com/serenity/integration/IntegrationApplication.java
@@ -142,7 +142,7 @@ observationService.getLegacyObservations(10000);  */
 //referalService.migrateReferalThread(1000);
 //diagnosisService.migrateDiagReportThread(5000);
 
-observationService.getLegacyObservations(5000); 
+observationService.getLegacyObservations(50000); 
 observationService.migrateObservationThread(5000);
 //invoiceService.migrateinvoiceThread(10);
 //noteService.noteThread(4000);
