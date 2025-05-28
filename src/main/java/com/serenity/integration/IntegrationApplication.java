613c7f6e9ff0aeb93272231e3541e9db955e4888
Bryan Laryea <bry_lar@yahoo.com>
2025-05-28 21:11:43 +0000

    added cvariables to the service

--- src/main/java/com/serenity/integration/IntegrationApplication.java
+++ src/main/java/com/serenity/integration/IntegrationApplication.java
@@ -116,12 +116,12 @@ InvoiceService invoiceService;
 	LocalDateTime starts = LocalDateTime.now();
 	LocalDate date= LocalDate.now().minusDays(1);
 	System.err.println(date +" is ...");
-	chargeItemService.chargeThread(5000);
-noteService.noteThread(4000);
+	//chargeItemService.chargeThread(5000);
+//noteService.noteThread(4000);
 serviceRequestService.migrateThread(3000);
 invoiceService.migrateinvoiceThread(3000);
 dService.migrationThread(5000);
-
+medicalRequestService.saveMedicalRequestThread();
 	//allergyService.migrateAllergyThread(1000);
 //referalService.migrateReferalThread(1000);
 /* patientService.getLegacyAllPatients2(20000, 20000, date);
